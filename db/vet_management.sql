6e7ab76ce06e5d7b902c11993a0b4f995cc1f21d
David Hunter <codeclanstudent@CodeClans-MacBook-Pro.local>
2023-08-26 12:34:08 +0100

    vets table created

--- db/vet_management.sql
+++ db/vet_management.sql
@@ -1,4 +1,11 @@
+DROP TABLE IF EXISTS vets;
 DROP TABLE IF EXISTS pets;
+
+CREATE TABLE vets(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    position VARCHAR(255)
+);
 
 CREATE TABLE pets(
     id SERIAL PRIMARY KEY,
@@ -7,7 +14,10 @@ CREATE TABLE pets(
     species VARCHAR(255),
     owner VARCHAR(255),
     contact_no VARCHAR(255),
-    treatment_notes TEXT
+    treatment_notes TEXT,
+    vet_id INT NOT NULL REFERENCES vets(id)
 );
+
+
 
 
