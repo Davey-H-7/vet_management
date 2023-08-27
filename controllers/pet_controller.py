7b864638a4c7636b31555bc5f4407d2a8932d07c
David Hunter <codeclanstudent@codeclans-mbp.home>
2023-08-27 21:45:37 +0100

    pets index updated

--- controllers/pet_controller.py
+++ controllers/pet_controller.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template, Blueprint, redirect
 import repositories.pet_repository as pet_repository
+import repositories.vet_repository as vet_repository
 from models.vet import Vet
 
 pet_blueprint = Blueprint('pet', __name__)
 
 @pet_blueprint.route('/pets')
 def index():
-    pets = pet_repository.select_all
+    pets = pet_repository.select_all()
     return render_template('pets/index.html', title = 'Registered Pets', all_pets = pets)
+
+@pet_blueprint.route('/pets/<id>')
+def show (id):
+    pet = pet_repository.select(id)
+    vet = vet_repository.vet_for_pet(pet)
+    return render_template('pets/show.html', title = pet.name, vet = vet, pet = pet)
