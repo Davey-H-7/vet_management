3ca02dba70350fd6b803cbb3aa1b0642465486d7
David Hunter <codeclanstudent@codeclans-mbp.home>
2023-08-27 22:25:25 +0100

    save_pet route added

--- controllers/pet_controller.py
+++ controllers/pet_controller.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, Blueprint, redirect
+from flask import Flask, render_template, Blueprint, redirect, request
 import repositories.pet_repository as pet_repository
 import repositories.vet_repository as vet_repository
 from models.vet import Vet
+from models.pet import Pet
 
 pet_blueprint = Blueprint('pet', __name__)
 
@@ -20,3 +21,17 @@ def show (id):
 def add_pet():
     vets = vet_repository.select_all()
     return render_template('pets/new.html', title = 'Register New Pet', all_vets = vets)
+
+@pet_blueprint.route('/pets', methods =['POST'])
+def save_pet():
+    name = request.form['name']
+    dob = request.form['dob']
+    species = request.form['species']
+    owner = request.form['owner']
+    contact_no = request.form['contact_no']
+    treatment_notes = request.form['treatment_notes']
+    vet_id = request.form['vet_id']
+    vet = vet_repository.select(vet_id)
+    new_pet = Pet(name, dob, species, owner, contact_no, vet, treatment_notes)
+    pet_repository.save(new_pet)
+    return redirect ('/pets')
