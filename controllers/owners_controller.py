ace4c3e9be49adf4368f0103f6dec7ae41d5b59b
David Hunter <codeclanstudent@codeclans-mbp.home>
2023-08-29 18:31:01 +0100

    Owner controller added new and save routes

--- controllers/owners_controller.py
+++ controllers/owners_controller.py
@@ -16,3 +16,16 @@ def show (id):
     owner = owner_repository.select(id)
     pets = pet_repository.pets_for_owner(owner)
     return render_template('owners/show.html', title = owner.first_name, owner = owner, pets = pets)
+
+@owner_blueprint.route('/owners/new')
+def new_owner():
+    return render_template('owners/new.html', title = 'New Pet Owner')
+
+@owner_blueprint.route('/owners', methods =['POST'])
+def save_owner():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    contact_no = request.form['contact_no']
+    new_owner = Owner(first_name, last_name, contact_no)
+    owner_repository.save(new_owner)
+    return redirect ('/owners')
