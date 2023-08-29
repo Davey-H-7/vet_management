d230956c156a111d70d6cdc95459883da4b4b5c0
David Hunter <codeclanstudent@codeclans-mbp.home>
2023-08-29 18:22:14 +0100

    Added owner controller + index, show routes

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from controllers.vets_controller import vet_blueprint
 from controllers.pets_controller import pet_blueprint
+from controllers.owners_controller import owner_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(vet_blueprint)
 app.register_blueprint(pet_blueprint)
+app.register_blueprint(owner_blueprint)
 
 @app.route("/")
 def home():
---
+++ controllers/owners_controller.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, Blueprint, redirect, request
+import repositories.owner_repository as owner_repository
+import repositories.pet_repository as pet_repository
+
+from models.owner import Owner
+
+owner_blueprint = Blueprint('owner', __name__)
+
+@owner_blueprint.route('/owners')
+def index():
+    owners = owner_repository.select_all()
+    return render_template('owners/index.html', title = 'All Pet Owners', all_owners = owners)
+
+@owner_blueprint.route('/owners/<id>')
+def show (id):
+    owner = owner_repository.select(id)
+    pets = pet_repository.pets_for_owner(owner)
+    return render_template('owners/show.html', title = owner.first_name, owner = owner, pets = pets)
